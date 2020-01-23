d3a2b57d3d277ad6315839e166411803b5cbf1cd
Nicola Redfern <nikkijredfern@gmail.com>
2020-01-23 21:05:29 +1000

    Killing myself by coding late at night. Time to walk away, Nicola.

--- js_for_beginners_final_project/script.js
+++ js_for_beginners_final_project/script.js
@@ -5,5 +5,7 @@ function $(sel) {
 var headingOne = $("h1").textContent;
 var letters = [];
 
-[...headingOne].forEach(x => letters.push(`<h1 class="anmiLetter">${x}</h1>`));
+[...headingOne].forEach(x => letters.push(`<h1 class="anmiLetter" onmouseover="mouseOverFun()">${x}</h1>`));
 $(".row").innerHTML = letters.join("");
+
+$(".anmiLetter").addEventListener('mouseover', function(event) { event.target.style.color = black});
