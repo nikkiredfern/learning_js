29295bab5b74ad0bce5a2f898b19b7786b904c0b
Nicola Redfern <nikkijredfern@gmail.com>
2020-01-24 15:35:32 +1000

    Day 17. Finished the javascript for beginners final project and course.

--- js_for_beginners_final_project/script.js
+++ js_for_beginners_final_project/script.js
@@ -5,7 +5,14 @@ function $(sel) {
 var headingOne = $("h1").textContent;
 var letters = [];
 
-[...headingOne].forEach(x => letters.push(`<h1 class="anmiLetter" onmouseover="mouseOverFun()">${x}</h1>`));
+[...headingOne].forEach(x => letters.push(`<h1 class="anmiLetter">${x}</h1>`));
 $(".row").innerHTML = letters.join("");
 
-$(".anmiLetter").addEventListener('mouseover', function(event) { event.target.style.color = black});
+document.querySelectorAll(".anmiLetter").forEach(item => {
+  item.addEventListener("mouseover",function() {
+    this.classList.add('active');
+ });
+  item.addEventListener("mouseout",function() {
+    this.classList.remove('active');
+  });
+});
