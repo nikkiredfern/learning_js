7e691789fff9e1e737231763eee9227ac1d257ab
Nicola Redfern <nikkijredfern@gmail.com>
2020-02-09 06:34:22 +1000

    A function to lookup objects in javascript.

--- record_collection.js
+++ record_collection.js
@@ -1,43 +1,43 @@
-// Setup
-var collection = {
-  2548: {
-    album: "Slippery When Wet",
-    artist: "Bon Jovi",
-    tracks: [
-      "Let It Rock",
-      "You Give Love a Bad Name"
-    ]
-  },
-  2468: {
-    album: "1999",
-    artist: "Prince",
-    tracks: [
-      "1999",
-      "Little Red Corvette"
-    ]
-  },
-  1245: {
-    artist: "Robert Palmer",
-    tracks: [ ]
-  },
-  5439: {
-    album: "ABBA Gold"
-  }
-};
+//Setup
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+];
 
+
+function lookUpProfile(name, prop){
 // Only change code below this line
-function updateRecords(id, prop, value) {
-  if (value == "") {
-    delete collection[id][prop];
-  } else if (prop == "tracks") {
-    collection[id][prop] = collection[id][prop] || [];
-    collection[id][prop].push(value);
-  } else {
-    collection[id][prop] = value;
-  }
+    for (var i=0; i < contacts.length; i++) {
+        if (contacts[i].firstName == name) {
+            return contacts[i][prop] || "No such property";
+        }
+    }
+return "No such contact";
 
-  return collection;
+// Only change code above this line
 }
 
-// Alter values below to test your code
-updateRecords(5439, "artist", "ABBA");
+// Change these values to test your function
+console.log(lookUpProfile("Akira", "likes"));
