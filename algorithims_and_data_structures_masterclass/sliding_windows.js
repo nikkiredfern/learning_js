cb8c88818bed07379efeee3a4575a5b830464653
Nicola Redfern <nikkijredfern@gmail.com>
2020-07-24 11:19:52 +1000

    cleaned up the learning_js folder. Completed WesBos drum kit.

---
+++ algorithims_and_data_structures_masterclass/sliding_windows.js
@@ -0,0 +1,10 @@
+// Sliding Window
+//
+// This pattern involves creating a window whic can either be an array
+// or number from one position to another.
+//
+// Depending on a certain condition, the window either increases or
+// closes (and a new window is created).
+//
+// Very useful foor keeping a track of a subset of data in an array/string etc.
+//
